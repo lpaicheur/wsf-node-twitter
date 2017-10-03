d1632b129d23436b2e98fa225a27deb412078371
lpaicheur <lpaicheur@users.noreply.github.com>
2017-10-03 11:54:19 +0200

    Test with .env

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const db = require('knex')({
   client: 'pg',
-  connection: process.env.DATABASE_URL,
+  connection: 'postgres://rdmfcewpwarisj:214f4d057f99c0c35b671a7c4c61d0041b17d8a677547b21c7368a3b3b63cfb0@ec2-54-75-228-125.eu-west-1.compute.amazonaws.com:5432/da0fsedudip1ah?ssl=true',
 });
 
 const app = express();
