586fe9f04b973381a59302a552e93644578122e5
lpaicheur <lpaicheur@users.noreply.github.com>
2017-10-04 11:59:44 +0200

    Rename id to user_id

--- app/components/users/getInfoByUser.js
+++ app/components/users/getInfoByUser.js
@@ -2,14 +2,14 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(`../../config/${env}`); // eslint-disable-line import/no-dynamic-require
 
 module.exports = (req, res) => {
-  if (!req.params.id || typeof req.params.id !== 'string') {
+  if (!req.params.user_id || typeof req.params.user_id !== 'string') {
     res.json({
       errors: ['user_id is not valid'],
       data: {},
     });
   }
 
-  config.DB('users').select('username', 'email', 'first_name', 'last_name', 'created_at').where('id', req.params.id)
+  config.DB('users').select('username', 'email', 'first_name', 'last_name', 'created_at').where('id', req.params.user_id)
     .then(users => res.json({
       errors: [],
       data: users,
