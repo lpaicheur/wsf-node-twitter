0d018670e37a59c5a49d3043fbbd277463bb81a9
lpaicheur <lpaicheur@users.noreply.github.com>
2017-10-15 01:01:53 +0200

    Use leftjoin

--- app/components/likes/getLikesByUser.js
+++ app/components/likes/getLikesByUser.js
@@ -18,35 +18,52 @@ module.exports = (req, res) => {
       'likes.created_at as likes_created_at',
       'tweets.message',
       'tweets.user_id',
-      'tweets.retweeted_from',
       'tweets.created_at',
+      'tweets.retweeted_from',
       'users.username',
       'users.first_name',
       'users.last_name',
+      'retweets.message as retweets_message',
+      'retweets.created_at as retweets_created_at',
+      'retweets.user_id as retweets_user_id',
     )
-    .join('tweets', 'likes.tweet_id', 'tweets.id')
-    .join('users', 'tweets.user_id', 'users.id')
+    .leftJoin('tweets', 'likes.tweet_id', 'tweets.id')
+    .leftJoin('users', 'tweets.user_id', 'users.id')
     .where('likes.user_id', req.params.user_id)
     .then(function (rows) {
-      const likes = rows.map(row => ({
-        id: row.id,
-        created_at: row.likes_created_at,
-        tweet: {
-          id: row.tweet_id,
-          created_at: row.created_at,
-          message: row.message,
-          user: {
-            id: row.user_id,
-            username: row.username,
-            last_name: row.last_name,
-            first_name: row.first_name,
+      console.log(rows);
+      const likes = rows.map((row) => {
+        let retweet = null;
+        if (row.retweeted_from) {
+          retweet = {
+            tweet_id: row.retweeted_from,
+            message: row.retweets_message,
+            created_at: row.retweets_created_at,
+            user: {
+              user_id: row.retweets_user_id,
+            },
+          };
+        }
+        return {
+          like_id: row.id,
+          created_at: row.likes_created_at,
+          tweet: {
+            tweet_id: row.tweet_id,
+            message: row.message,
+            created_at: row.created_at,
+            user: {
+              user_id: row.user_id,
+              username: row.username,
+              last_name: row.last_name,
+              first_name: row.first_name,
+            },
+            retweeted_from: retweet,
           },
-          retweeted_from: row.retweeted_from,
-        },
-      }));
+        };
+      });
       const data = {
         user: {
-          id: req.params.user_id,
+          user_id: req.params.user_id,
         },
         likes,
       };
